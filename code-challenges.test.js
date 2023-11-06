4f9d6a6bf67baeb6ecef750d245e164127751937
alexnguyen <alexanderdnguyen4@gmail.com>
2023-11-06 04:14:37 -0800

    Finished assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+const { assertParenthesizedExpression } = require("@babel/types")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -20,9 +22,40 @@ const colors1 = ["purple", "blue", "green", "yellow", "pink"]
 const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+// describe("arrayShuffle", () => {
+//     it("Takes in an array, removes the first item from the array and shuffles the remaining items.", () => {
+//         expect(arrayShuffle(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+//         expect(arrayShuffle(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+//     })
+// })
+
 // b) Create the function that makes the test pass.
 
+const arrayShuffle = (array) => {
+    array.shift()
+    let newArr = []
+    let arrIndex = []
+
+    while (arrIndex.length < array.length) {
+    let randomIndex = Math.floor(Math.random() * array.length)
+    
+    if (!arrIndex.includes(randomIndex)) {
+        arrIndex.push(randomIndex)
+    }
+}
+    for (let i = 0; i < arrIndex.length; i++) {
+        newArr.push(array[arrIndex[i]])
+    }
+    return newArr
+}
+console.log(arrayShuffle(colors1))
+console.log(arrayShuffle(colors2))
+
 // Pseudo code:
+
+// Input: an array
+// Output: a new array that has removed the first element from the original array and shuffled the remaining elements
+// Create a function that takes in an array as the parameter. Use .shift() method to remove the first element from the array. Use a while loop to generate an array of random indices. Use a for loop to push the remaining elements from the original array into a new array according to random indices. 
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
 
@@ -33,6 +66,23 @@ const votes1 = { upVotes: 13, downVotes: 2 }
 const votes2 = { upVotes: 2, downVotes: 33 }
 // Expected output: -31
 
+describe("voteTally", () => {
+    it("Takes in an object that contains up votes and down votes and returns the end tally.", () => {
+        expect(voteTally(votes1)).toEqual(11)
+        expect(voteTally(votes2)).toEqual(-31)
+    })
+})
+
 // b) Create the function that makes the test pass.
 
+const voteTally = (object) => {
+    return object.upVotes - object.downVotes
+}
+console.log(voteTally(votes1))
+console.log(voteTally(votes2))
+
 // Pseudo code:
+
+// Input: an object containing up votes and down votes
+// Output: the difference between the number of up votes and down votes
+// Create a function that takes in an object as the parameter. Have the function access the values for up votes and down votes. Calculate the difference between the up votes and down votes.
--- interview-questions.md
+++ interview-questions.md
@@ -8,32 +8,32 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the virtual DOM in React?
 
-Your answer:
+Your answer: The virtual DOM in React is the visual representation of the code on the web page.
 
-Researched answer:
+Researched answer: The virtual DOM is a JavaScript representation of the Document Object Model within a framework such as React.
 
 2. Ruby has an implicit return. What does that mean?
 
-Your answer:
+Your answer: The code executes without the need for specific returns being delineated.
 
-Researched answer:
+Researched answer: Every code block in Ruby returns the value of the last line automatically without the word return being expressly written.
 
 3. Compare and contrast JavaScript and Ruby. What are three things they have in common and what are three differences?
 
-Your answer:
+Your answer: JavaScript and Ruby are both scripting languages, dynamic, and open source. Ruby is object-oriented while JavaScript uses both objects and functions. JavaScript has type coercion whereas Ruby does not. Variables are declared in JavaScript but not in Ruby.
 
-Researched answer:
+Researched answer: JavaScript and Ruby are both scripting languages, dynamic, and can use object-oriented programming. JavaScript is for front and back-end development while Ruby is strictly for back-end development. Ruby's syntax is much simpler and straightforward compared to JavaScript. Ruby can monitor memory utilization whereas JavaScript cannot.
 
 4. STRETCH: What is a version manager?
 
-Your answer:
+Your answer: A version manager is a dependency that organizes and maintains code at the user's discretion.
 
-Researched answer:
+Researched answer: A version manager is a special program that handles updating and being able to switch between different versions of coding languages.
 
 ## Looking Ahead: Terms for Next Week
 
-1. RSpec:
+1. RSpec: a testing tool for Ruby
 
-2. Test-driven development:
+2. Test-driven development: the software development process of requiring software to fit test cases before being fully developed
 
-3. CRUD: 
+3. CRUD: create, read, update, and delete
